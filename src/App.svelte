c9158f9e937655e73ad15e9c4c0fbeea56cbebce
Kilian Domaratius <github@tequilian.de>
2021-04-29 14:16:17 +0200

    Add ibm carbon

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,29 @@
 <script>
+  import games from "./games.json";
+  import { Grid, Row, Column } from "carbon-components-svelte";
+
+  /** @type {"white" | "g10" | "g90" | "g100"} */
+  let theme;
+  $: document.documentElement.setAttribute("theme", theme);
+
+  /**
+   * Sets theme color based on user preferences
+   */
+  function setTheme() {
+    theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "g100"
+      : "white";
+  }
+
+  // set theme on mode change
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", () => {
+      setTheme();
+    });
+
+  // init theme
+  setTheme();
 </script>
 
 <main>
